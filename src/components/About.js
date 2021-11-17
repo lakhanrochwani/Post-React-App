09be13c31c8e73fa3fa8bd92658f867a08e0f18f
lakhanrochwani <rochwanilakhan@gmail.com>
2021-11-17 17:51:48 -0600

    4 files modified

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function About() {
   return (
-    <main>
-      <h1>About</h1>
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: '1rem' }}>This blog app is a project.</p>
     </main>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Header({ title }) {
+  let navigate = useNavigate();
   return (
     <header className="Header">
-      <h1>{title}</h1>
+      <h1 onClick={() => navigate('/')}>{title}</h1>
     </header>
   );
 }
